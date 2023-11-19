ecb733a840a287a85eb68d826298ef4a77677faa
Dark-Explorer <diemannschaft2003@gmail.com>
2023-11-19 16:49:05 +0700

    added Maze setup with GUI (beta)

--- Assignment for AI Intro/Official version/Maze.py
+++ Assignment for AI Intro/Official version/Maze.py
@@ -1,31 +1,32 @@
-import turtle
+import turtle  # import turtle library
 import time
 import sys
 from collections import deque
 
-wn = turtle.Screen()
-wn.bgcolor("black")
+wn = turtle.Screen()  # define the turtle screen
+wn.bgcolor("black")  # set the background colour
 wn.title("Maze Solving Program")
-wn.setup(1300, 700)
+wn.setup(1300, 700)  # setup the dimensions of the working window
 
 
-class Maze(turtle.Turtle):
+# this is the class for the Maze
+class Maze(turtle.Turtle):  # define a Maze class
     def __init__(self):
         turtle.Turtle.__init__(self)
-        self.shape("square")
-        self.color("white")
-        self.penup()
-        self.speed(0.5)
+        self.shape("square")  # the turtle shape
+        self.color("white")  # colour of the turtle
+        self.penup()  # lift the pen so it do not leave a trail
+        self.speed(0)
 
 
-# Path found by algorithm(s)
+# this is the class for the finish line - green square in the maze
 class Green(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
         self.color("green")
         self.penup()
-        self.speed(0.5)
+        self.speed(0)
 
 
 class Blue(turtle.Turtle):
@@ -34,17 +35,17 @@ class Blue(turtle.Turtle):
         self.shape("square")
         self.color("blue")
         self.penup()
-        self.speed(0.5)
+        self.speed(0)
 
 
-# Agent class
+# this is the class for the yellow or turtle
 class Red(turtle.Turtle):
     def __init__(self):
         turtle.Turtle.__init__(self)
         self.shape("square")
         self.color("red")
         self.penup()
-        self.speed(0.5)
+        self.speed(0)
 
 
 class Yellow(turtle.Turtle):
@@ -53,46 +54,29 @@ class Yellow(turtle.Turtle):
         self.shape("square")
         self.color("yellow")
         self.penup()
-        self.speed(0.5)
+        self.speed(0)
 
-
-grid = [
-"+++++++++++++++",
-"+s+       + +e+",
-"+ +++++ +++ + +",
-"+ + +       + +",
-"+ +   +++ + + +",
-"+ + + +   + + +",
-"+   + +   + + +",
-"+++++ +   + + +",
-"+     +   +   +",
-"+++++++++++++++",
-]
-
-# grid = [
-# "+++++++++",
-# "+ ++s++++",
-# "+ ++ ++++",
-# "+ ++ ++++",
-# "+    ++++",
-# "++++ ++++",
-# "++++ ++++",
-# "+      e+",
-# "+++++++++",
-# ]
 
 # grid = [
 # "+++++++++++++++",
-# "+             +",
-# "+             +",
-# "+             +",
-# "+     e       +",
-# "+             +",
-# "+             +",
-# "+             +",
-# "+ s           +",
+# "+s+       + +e+",
+# "+ +++++ +++ + +",
+# "+ + +       + +",
+# "+ +   +++ + + +",
+# "+ + + +   + + +",
+# "+   + +   + + +",
+# "+++++ +   + + +",
+# "+     +   +   +",
 # "+++++++++++++++",
 # ]
+
+with open('grid.txt', 'r') as f:
+    grid = []
+    for line in f:
+        row = line.strip()
+        grid.append(row)
+
+print(grid)
 # grid = [
 #     "+++++++++++++++++++++++++++++++++++++++++++++++++++",
 #     "+               +                                 +",
@@ -138,32 +122,33 @@ grid = [
 #     "s                  ++",
 #     "+++++++++++++++++++++",
 # ]
+# print(grid)
 
-def setup_maze(grid):
-    global start_x, start_y, end_x, end_y
-    for y in range(len(grid)):
-        for x in range(len(grid[y])):
-            character = grid[y][x]
-            screen_x = -588 + (x * 24)
-            screen_y = 288 - (y * 24)
+def setup_maze(grid):  # define a function called setup_maze
+    global start_x, start_y, end_x, end_y  # set up global variables for start and end locations
+    for y in range(len(grid)):  # read in the grid line by line
+        for x in range(len(grid[y])):  # read each cell in the line
+            character = grid[y][x]  # assign the variable "character" the x and y location on the grid
+            screen_x = -588 + (x * 24)  # move to the x location on the screen staring at -588
+            screen_y = 288 - (y * 24)  # move to the y location of the screen starting at 288
 
             if character == "+":
-                maze.goto(screen_x, screen_y)
-                maze.stamp()
-                walls.append((screen_x, screen_y))
+                maze.goto(screen_x, screen_y)  # move pen to the x and y location and
+                maze.stamp()  # stamp a copy of the turtle on the screen
+                walls.append((screen_x, screen_y))  # add coordinate to walls list
 
             if character == " " or character == "e":
-                path.append((screen_x, screen_y))
+                path.append((screen_x, screen_y))  # add " " and e to path list
 
             if character == "e":
                 green.color("purple")
-                green.goto(screen_x, screen_y)
-                end_x, end_y = screen_x, screen_y
+                green.goto(screen_x, screen_y)  # send green sprite to screen location
+                end_x, end_y = screen_x, screen_y  # assign end locations variables to end_x and end_y
                 green.stamp()
                 green.color("green")
 
             if character == "s":
-                start_x, start_y = screen_x, screen_y
+                start_x, start_y = screen_x, screen_y  # assign start locations variables to start_x and start_y
                 red.goto(screen_x, screen_y)
 
 
@@ -176,28 +161,28 @@ def bfs(x, y):
     frontier.append((x, y))
     solution[x, y] = x, y
 
-    while len(frontier) > 0:
-        time.sleep(0.9)
-        x, y = frontier.popleft()
+    while len(frontier) > 0:  # exit while loop when frontier queue equals zero
+        time.sleep(0.1)
+        x, y = frontier.popleft()  # pop next entry in the frontier queue an assign to x and y location
 
-        if (x - 24, y) in path and (x - 24, y) not in visited:
+        if (x - 24, y) in path and (x - 24, y) not in visited:  # check the cell on the left
             cell = (x - 24, y)
-            solution[cell] = x, y
-            blue.goto(cell)
+            solution[cell] = x, y  # backtracking routine [cell] is the previous cell. x, y is the current cell
+            blue.goto(cell)  # identify frontier cells
             blue.stamp()
-            frontier.append(cell)
-            visited.add((x - 24, y))
+            frontier.append(cell)  # add cell to frontier list
+            visited.add((x - 24, y))  # add cell to visited list
 
-        if (x, y - 24) in path and (x, y - 24) not in visited:
+        if (x, y - 24) in path and (x, y - 24) not in visited:  # check the cell down
             cell = (x, y - 24)
             solution[cell] = x, y
             blue.goto(cell)
             blue.stamp()
             frontier.append(cell)
             visited.add((x, y - 24))
-            #print(solution)
+            print(solution)
 
-        if (x + 24, y) in path and (x + 24, y) not in visited:
+        if (x + 24, y) in path and (x + 24, y) not in visited:  # check the cell on the  right
             cell = (x + 24, y)
             solution[cell] = x, y
             blue.goto(cell)
@@ -205,7 +190,7 @@ def bfs(x, y):
             frontier.append(cell)
             visited.add((x + 24, y))
 
-        if (x, y + 24) in path and (x, y + 24) not in visited:
+        if (x, y + 24) in path and (x, y + 24) not in visited:  # check the cell up
             cell = (x, y + 24)
             solution[cell] = x, y
             blue.goto(cell)
@@ -223,15 +208,15 @@ def dfs(x, y):
     while len(frontier) > 0:
         time.sleep(0.1)
         x, y = frontier.pop()
-        if (x - 24, y) in path and (x - 24, y) not in visited:
+        if (x - 24, y) in path and (x - 24, y) not in visited:  # check the cell on the left
             cell = (x - 24, y)
-            solution[cell] = x, y
-            blue.goto(cell)
+            solution[cell] = x, y  # backtracking routine [cell] is the previous cell. x, y is the current cell
+            blue.goto(cell)  # identify frontier cells
             blue.stamp()
-            frontier.append(cell)
-            visited.add((x - 24, y))
+            frontier.append(cell)  # add cell to frontier list
+            visited.add((x - 24, y))  # add cell to visited list
 
-        if (x, y - 24) in path and (x, y - 24) not in visited:
+        if (x, y - 24) in path and (x, y - 24) not in visited:  # check the cell down
             cell = (x, y - 24)
             solution[cell] = x, y
             blue.goto(cell)
@@ -240,7 +225,7 @@ def dfs(x, y):
             visited.add((x, y - 24))
             print(solution)
 
-        if (x + 24, y) in path and (x + 24, y) not in visited:
+        if (x + 24, y) in path and (x + 24, y) not in visited:  # check the cell on the  right
             cell = (x + 24, y)
             solution[cell] = x, y
             blue.goto(cell)
@@ -248,7 +233,7 @@ def dfs(x, y):
             frontier.append(cell)
             visited.add((x + 24, y))
 
-        if (x, y + 24) in path and (x, y + 24) not in visited:
+        if (x, y + 24) in path and (x, y + 24) not in visited:  # check the cell up
             cell = (x, y + 24)
             solution[cell] = x, y
             blue.goto(cell)
@@ -261,10 +246,10 @@ def dfs(x, y):
 def backRoute(x, y):
     yellow.goto(x, y)
     yellow.stamp()
-    while (x, y) != (start_x, start_y):
-        yellow.goto(solution[x, y])
+    while (x, y) != (start_x, start_y):  # stop loop when current cells == start cell
+        yellow.goto(solution[x, y])  # move the yellow sprite to the key value of solution ()
         yellow.stamp()
-        x, y = solution[x, y]
+        x, y = solution[x, y]  # "key value" now becomes the new key
 
 
 # set up classes
@@ -274,14 +259,15 @@ blue = Blue()
 green = Green()
 yellow = Yellow()
 
+# setup lists
 walls = []
 path = []
 visited = set()
 frontier = deque()
-solution = {}
+solution = {}  # solution dictionary
 
-# Main program
+# main program starts here ####
 setup_maze(grid)
-bfs(start_x, start_y)
+dfs(start_x, start_y)
 backRoute(end_x, end_y)
 wn.exitonclick()
